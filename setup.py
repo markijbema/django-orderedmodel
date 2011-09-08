46b28ef5be862fca0cb09c211e0cb82285ad9eec
Mark IJbema <markijbema@gmail.com>
2011-09-08 14:28:25 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-orderedmodel',
+    version = '0.1',
+    description = 'A generic class for Models with Order.',
+    #author = '',
+    #author_email = '',
+    url = 'https://github.com/markijbema/django-orderedmodel',
+    license = 'MIT',
+    packages = find_packages(),
+)
